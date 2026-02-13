113d07edd4253f824f4a02abd1df58ce7022e608
Katie Lin <katielin019@gmail.com>
2026-02-13 15:55:58 -0800

    move files around, make svgs into svelte components

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
-    import Grid from "./Grid.svelte";
-    import { levelData, SHAPES } from "./data.js";
+    import Grid from "./lib/Grid.svelte";
+    import { levelData, SHAPES } from "./lib/data.js";
 
     const ROWS = levelData.rows;
     const COLS = levelData.cols;
@@ -31,21 +31,3 @@
 <div id="app">
     <Grid cells={grid} width={COLS} onCellClick={rotate} />
 </div>
-
-<style>
-    :global(body) {
-        margin: 0;
-        background: #1a1a1a;
-        font-family:system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        font-size: 1rem;
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-        height: 100vh;
-        width: 100vw;
-    }
-
-    #app {
-        padding: 2rem;
-    }
-</style>
---
+++ src/lib/tiles/Source.svelte
@@ -0,0 +1,4 @@
+<script>
+    let { rotation = 0, waterColor = "#0288D1"} = $props();
+    
+</script>
